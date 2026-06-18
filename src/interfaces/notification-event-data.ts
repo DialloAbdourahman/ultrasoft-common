0856dcd4d966e2b615ed9405d403a359dd5daf9d
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2026-06-18 19:51:04 +0100

    fix

--- src/interfaces/notification-event-data.ts
+++ src/interfaces/notification-event-data.ts
@@ -1,11 +1,3 @@
-import { EnumNotificationStatus } from "../enums/notification-status";
-import { EnumNotificationType } from "../enums/notification-type";
-
 export interface NotificationEventData {
   id: string;
-  type: EnumNotificationType;
-  recipient: string;
-  subject: string;
-  message: string;
-  status: EnumNotificationStatus;
 }
